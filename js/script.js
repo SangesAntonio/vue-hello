c6cf0998902c5565c4678b8ac3e14c2981bd9cb5
SangesAntonio <sanges65@gmail.com>
2022-01-26 15:47:45 +0100

    added title adn img with vue.js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,15 @@
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.*/
 
-vue.config.devtools = true; 
+//vue.config.devtools = true; 
+
+console.log('vue ok', Vue);
+console.log('js ok');
+
+const app = new Vue({
+    el:'#root',
+    data:{
+        message: 'hello word',
+        media:'https://www.lescienze.it/images/2018/07/09/195104163-9fbe7398-f4e4-4885-ae7b-d7179a0719eb.jpg',
+    }
+})
